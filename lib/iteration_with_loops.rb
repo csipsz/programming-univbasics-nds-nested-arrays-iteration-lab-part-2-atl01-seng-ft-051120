2fcec1532825cc157ab66940544c2a40dcde86fb
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-30 23:16:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,9 @@ def find_min_in_nested_arrays(src)
   while row_count < src.length do 
     column_count = 0 
     while column_count < src[row_count].length do
-      min = 0 
-      if src[row_count][column_count] 
+      min = src[row_count][0] 
+      if src[row_count][column_count] < min
+        min = 
       column_count += 1
     end
     row_count += 1
